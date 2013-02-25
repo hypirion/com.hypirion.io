7015979f4878712a833fd5843ae1fbb871fcf3e4
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-25 20:07:04 +0100

    Start on thread-safe killing.

--- src/com/hypirion/io/RevivableInputStream.java
+++ src/com/hypirion/io/RevivableInputStream.java
@@ -2,14 +2,27 @@ package com.hypirion.io;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 
 public class RevivableInputStream extends InputStream {
     protected boolean killed;
     protected InputStream in;
 
+    protected boolean streamClosed;
+    protected byte data;
+    protected volatile boolean newData;
+
+    protected final Object lock;
+    protected final RevivableReader reader;
+
     public RevivableInputStream(InputStream in) {
         this.in = in;
         killed = false;
+        streamClosed = false;
+        newData = false;
+        lock = new Object();
+        reader = new RevivableReader(this);
+        // Fire up new thread around here.
     }
 
     public int available() throws IOException {
@@ -24,25 +37,60 @@ public class RevivableInputStream extends InputStream {
         return in.markSupported();
     }
 
-    public int read() throws IOException {
-        if (killed)
-            return -1;
-        else
-            return in.read();
+    public synchronized int read() throws IOException {
+        synchronized (lock) {
+            if (killed || streamClosed)
+                return -1;
+            try {
+                lock.wait();
+            }
+            catch (InterruptedException ie) {
+                throw new InterruptedIOException();
+            }
+            if (killed || streamClosed)
+                return -1;
+            int val = data;
+            // Some startup of the reader here
+            return data;
+        }
     }
 
-    public int read(byte[] b) throws IOException {
-        if (killed)
-            return -1;
-        else
-            return in.read(b);
+    public synchronized int read(byte[] b) throws IOException {
+        synchronized (lock) {
+            if (killed || streamClosed)
+                return -1;
+            try {
+                lock.wait();
+            }
+            catch (InterruptedException ie) {
+                throw new InterruptedIOException();
+            }
+            if (killed || streamClosed || data == -1)
+                return -1;
+            b[0] = data;
+            // Some startup of the reader here
+            return 1;
+        }
     }
 
-    public int read(byte[] b, int off, int len) throws IOException {
-        if (killed)
-            return -1;
-        else
-            return in.read(b, off, len);
+    public synchronized int read(byte[] b, int off, int len) throws IOException{
+        synchronized (lock) {
+            if (killed || streamClosed)
+                return -1;
+            try {
+                lock.wait();
+            }
+            catch (InterruptedException ie) {
+                throw new InterruptedIOException();
+            }
+            if (killed || streamClosed || data == -1)
+                return -1;
+            if (len == 0)
+                return 0;
+            b[off] = data;
+            // Some startup of the reader here
+            return 1;
+        }
     }
 
     public void reset() throws IOException {
@@ -54,10 +102,24 @@ public class RevivableInputStream extends InputStream {
     }
 
     public void kill() {
-        killed = true;
+        synchronized (lock) {
+            killed = true;
+            lock.notifyAll();
+        }
     }
 
-    public void ressurect() {
+    public synchronized void ressurect() {
         killed = false;
+    }
+
+    private class RevivableReader implements Runnable {
+        public RevivableReader(RevivableInputStream ris) {
+            // Do some magic here
+        }
+
+        @Override
+        public void run() {
+            // And here. obviously.
+        }
     }
 }
