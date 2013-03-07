1fa175b5b693a8e7abc19f475da95ddc15e92477
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-07 15:56:58 +0100

    Skeleton for Pipe.

---
+++ src/com/hypirion/io/Pipe.java
@@ -0,0 +1,47 @@
+package com.hypirion.io;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Pipe {
+
+    private final Thread threadPumper;
+    private final PipeThread pt;
+    private final InputStream in;
+    private final OutputStream out;
+
+    public Pipe(InputStream in, OutputStream out) {
+        this.in = in;
+        this.out = out;
+        pt = new PipeThread();
+        threadPumper = new Thread(pt);
+        threadPumper.setName(String.format("PipeThread %s %s", in.hashCode(),
+                                           out.hashCode()));
+        threadPumper.setDaemon(true);
+    }
+
+    public void join() throws InterruptedException {
+        threadPumper.join();
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+        stop(true);
+    }
+
+    public void stop(boolean block) {
+
+    }
+
+    private class PipeThread implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                
+            }
+        }
+    }
+}
