d3b444870175e31524833b96c230d33fde1d8f30
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-15 18:07:10 +0100

    Add data buffer.

--- src/com/hypirion/io/Pipe.java
+++ src/com/hypirion/io/Pipe.java
@@ -5,15 +5,23 @@ import java.io.OutputStream;
 
 public class Pipe {
 
+    public static final int DEFAULT_BUFFER_SIZE = 1024;
+
     private final Thread threadPumper;
     private final PipeThread pt;
     private final InputStream in;
     private final OutputStream out;
+    private final byte[] data;
     private final Object readLock, writeLock;
 
     public Pipe(InputStream in, OutputStream out) {
+        this(in, out, DEFAULT_BUFFER_SIZE);
+    }
+
+    public Pipe(InputStream in, OutputStream out, int bufsize) {
         this.in = in;
         this.out = out;
+        data = new byte[bufsize];
         pt = new PipeThread();
         threadPumper = new Thread(pt);
         threadPumper.setName(String.format("PipeThread %s %s", in.hashCode(),
