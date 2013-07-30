7fe91ae1f07f6d29910ff57378791fa694cec810
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-30 11:44:39 +0200

    Ensure we're compiling for 1.6.

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths []
   :java-source-paths ["src"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :deploy-branches ["stable"]
   :profiles
   {:dev {:plugins [[lein-shell "0.2.0"]]
-         :aliases {"javadoc" ["shell" "javadoc" "-d" "0.3.0-SNAPSHOT"
+         :aliases {"javadoc" ["shell" "javadoc" "-d" "0.4.0-SNAPSHOT"
                               "-sourcepath" "src/" "com.hypirion.io"]}}})
